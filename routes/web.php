bbec981353ad8b231461f47d56dd6088ec61afdd
Romario Huebra <romarioj2h@gmail.com>
2018-02-13 11:06:36 -0300

    Correccion en la ruta del admin

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/admin', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('/admin')->group(function() {
     Route::prefix('/contactos')->group(function () {
