fdd34951afab367c93de31493ff6fdd880ab591d
Romario Huebra <romarioj2h@gmail.com>
2018-02-16 19:41:52 -0300

    Cambiando tema del sitio! *cdn

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
 
     <title>{{ config('app.name', 'Preguntas') }}</title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/sketchy/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
