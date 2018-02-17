87b3436648659ab918d72880204e30e55b5cc6fd
Romario Huebra <romarioj2h@gmail.com>
2018-02-17 11:38:24 -0300

    Diseño inicial para la pagina de servicios

--- app/Http/Controllers/Admin/ServiciosController.php
+++ app/Http/Controllers/Admin/ServiciosController.php
@@ -36,4 +36,48 @@ class ServiciosController extends Controller
         Servicios::findOrFail($id)->delete();
         return redirect()->route('admin.servicios.index');
     }
+
+    public function guardar(Request $request)
+    {
+        if ($request->isMethod('post') || $request->isMethod('patch')) {
+            if ($request->isMethod('post')) {
+                $this->validate($request, $this->obtenerReglasValidacion());
+                $servicio = new Servicios();
+            } else {
+                $id = $request->input('id');
+                $this->validate($request, $this->obtenerReglasValidacion($id));
+                $servicio = Servicios::findOrFail($id);
+            }
+
+            $servicio->nombre = $request->input('nombre');
+            $servicio->descripcion = $request->input('descripcion');
+            $servicio->telefonos = $request->input('telefonos');
+            $servicio->direccion = $request->input('direccion');
+            $servicio->localidad = $request->input('localidad');
+            $servicio->serviciosGruposId = $request->input('serviciosGruposId');
+            $servicio->save();
+            $request->session()->flash('mensage', [
+                'tipo' => 'success',
+                'mensage' => 'Servicio salvo con éxito!'
+            ]);
+            return redirect()->route('admin.servicios.index');
+        } else {
+            return view('admin.servicios.agregar');
+        }
+    }
+
+    private function obtenerReglasValidacion($id = null)
+    {
+        $reglas = [
+            'nombre' => 'required',
+            'telefonos' => 'required',
+            'localidad' => 'required',
+            'serviciosGruposId' => 'required',
+        ];
+
+        if ($id) {
+            $reglas['id'] = 'required|integer';
+        }
+        return $reglas;
+    }
 }
--- resources/views/servicios/obtenerServicios.blade.php
+++ resources/views/servicios/obtenerServicios.blade.php
@@ -7,7 +7,20 @@
 @section('content')
     <div class="list-group">
         @foreach($grupo->servicios as $servicio)
-            <a href="#" class="list-group-item list-group-item-action">{{ $servicio->nombre }}</a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ $servicio->nombre }}</h5>
+                    <small style="color: #e9cc14">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </small>
+                </div>
+                <p class="mb-1">{{ $servicio->descripcion }}</p>
+                <small>{{ $servicio->localidad }} - <i class="fab fa-whatsapp"></i></small>
+            </a>
         @endforeach
     </div>
 @endsection
