f67dbc5a56a012e00bd1c961bd6e4ea84cf6b3ca
Romario Huebra <romarioj2h@gmail.com>
2018-04-07 09:16:16 -0300

    menu mas claro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                     <ul class="nav navbar-nav">
                         @if(!Auth::guest())
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Grupos <span class="caret"></span></a>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Grupos Preguntas <span class="caret"></span></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="{{ route('admin.grupos.index') }}">Listado</a></li>
                                 </ul>
